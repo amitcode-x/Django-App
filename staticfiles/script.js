de173e7a5671c812ddaaee7d48623c302f31202d
amitcoderhub <amitchauhan6599@gmail.com>
2025-10-29 22:13:35 +0530

    push

---
+++ staticfiles/script.js
@@ -0,0 +1,8 @@
+
+  const menu = document.getElementById("mobile-menu");
+  const navLinks = document.getElementById("nav-links");
+
+  menu.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+
